ff4ffe4525ca63b5c53a55d0e1643c7cbe30d712
nataliya <n.m.suvorova@mail.ru>
2022-06-27 23:07:05 +0300

    task3

---
+++ task3_hw/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Prompt(string message){
+    Console.Write (message);
+    int a = int.Parse (Console.ReadLine ());
+    return a;
+}
+
+int A = Prompt("Введите любое число: ");
+
+//if (A<1)
+
+while (A>999) {
+    A=A/10;
+}
+
+if (A>99) {
+Console.WriteLine(A%10);
+}
+else Console.WriteLine("Третьей цифры нет");
