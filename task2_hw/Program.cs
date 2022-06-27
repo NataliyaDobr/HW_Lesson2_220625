5962633a921e847381853d11c98220413fb5c385
nataliya <n.m.suvorova@mail.ru>
2022-06-27 22:42:15 +0300

    task 2

---
+++ task2_hw/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int Rnd (int min, int max){
+    Random rnd = new Random();
+    return rnd.Next(min, max+1);
+}
+
+int a = Rnd (100,999);
+Console.WriteLine(a);
+
+int temp1 = a%10;
+int temp2 = a/100;
+
+Console.Write(temp2);
+Console.WriteLine(temp1);
