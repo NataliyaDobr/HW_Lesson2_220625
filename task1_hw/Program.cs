1c7d981e9e49906a2724c6e3fd8ecb3de04f8faa
nataliya <n.m.suvorova@mail.ru>
2022-06-27 22:38:21 +0300

    task1

---
+++ task1_hw/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string message){
+    Console.Write (message);
+    int a = int.Parse (Console.ReadLine ());
+    return a;
+}
+
+int A = Prompt("Введите трехзначное число = ");
+
+if (A>99 && A<1000) {
+int temp = A/10;
+int secont_number = temp%10;
+
+Console.WriteLine(secont_number);
+}
+else Console.WriteLine("Введено не трехзначное число");
